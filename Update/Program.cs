a2ab3dcae72c56655f873d850a9308e4b0da054c
DemetriouS <dafatov@yandex.ru>
2021-07-27 22:08:28 +0300

    Добавьте файлы проекта.

---
+++ Update/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string process = args[0];
+
+                Console.WriteLine(process);
+                while (Process.GetProcessesByName(process).Length > 0)
+                {
+                    Process[] tmp = Process.GetProcessesByName(process);
+                    foreach (Process p in tmp)
+                    {
+                        Console.WriteLine(p.ProcessName + " Killed");
+                        p.Kill();
+                    }
+                    Thread.Sleep(300);
+                }
+                DateTime d = File.GetLastWriteTimeUtc(process + ".exe.part");
+                Console.WriteLine(d);
+
+                File.Move(process + ".exe.part", process + ".exe", true);
+                File.SetLastWriteTimeUtc(process + ".exe", d);
+
+                Process.Start(process + ".exe");
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+            Console.ReadKey(true);
+        }
+    }
+}
